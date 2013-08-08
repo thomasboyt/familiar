48f8f84c34950d2a38f7cdd09ea1f83ab9ec6976
unknown <jferguson2.718@gmail.com>
2013-08-08 15:21:02 -0400

    Revert "more internet"
    This reverts commit 9669d67ffdd9962e098082792a0f10850214aa99.

--- project.clj
+++ project.clj
@@ -6,10 +6,6 @@
                  [org.clojure/tools.namespace "0.2.3"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
                  [org.clojure/math.combinatorics "0.0.4"]
-                 [org.clojure/data.json "0.2.2"]
-                 [ring "1.2.0"]
-                 
-                 [compojure "1.1.5"]
                  [korma "0.3.0-RC5"]
                  [log4j "1.2.15" :exclusions [javax.mail/mail
                                               javax.jms/jms
@@ -20,6 +16,4 @@
                  [swiss-arrows "0.6.0"]
                  [clj-time "0.5.1"]
                  [jkkramer/loom "0.2.0"]]
-  :dev-dependencies [[lein-ring "0.4.0"]]
-  :main familiar.core
-  :ring {:handler familiar.core/app})
+  :main familiar.core)
